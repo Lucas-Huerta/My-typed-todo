b891f164b6bce039479c7e7806fbc589d3ed60f3
Cédric Jamme <cedricjamme@MacBook-Pro-de-Cedric.local>
2024-05-14 16:19:18 +0200

    add dueDate property dans interface

--- src/model/todo-item.ts
+++ src/model/todo-item.ts
@@ -1,10 +1,11 @@
+import { DueDate } from "./due-date";
 import { Todopriority } from "./todo-priority";
 import { TodoStatus } from "./todo-status";
 
 export interface TodoItem {
     id : number;
     description : string;
-    duDate : Date;
+    duDate : DueDate;
     status : TodoStatus
     priority : Todopriority
 }
